4f7135b0aa27cba2b253ec3cf37ea613498e7466
Aubrey Hewes <AubreyHewes@users.noreply.github.com>
2019-06-06 17:39:52 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,72 @@
+[![wercker status](https://app.wercker.com/status/446df94fe0566fad505da38719e7cce6/s/master "wercker status")](https://app.wercker.com/project/byKey/446df94fe0566fad505da38719e7cce6)
+
+# wercker-kelevra-slack-notifier
+
+A wercker after-step slack notifier written in bash and curl. Make sure you create a Slack webhook first (see the Slack integrations page to set one up).
+
+This is based on the [official build step](https://github.com/wercker/step-slack). Why? as we have added the `notify_on` option `failed_and_passed_after_failed`.
+
+# Options
+
+Options are applied via organisation environment, pipeline environment or the `wercker.yml`.
+
+|Option|Description|Environment|
+|---|---|---|
+|`url`|The Slack webhook URL (REQUIRED)<br/>The `url` parameter is the [slack webhook](https://api.slack.com/incoming-webhooks) that wercker should post to. You can create an _incoming webhook_ on your slack integration page.|`SLACK_NOTIFIER_URL`|
+|`channel`|The Slack channel (excluding `#`)|`SLACK_NOTIFIER_CHANNEL`|
+|`username`|Username of the notification message. i.e. who it is sent by.|`SLACK_NOTIFIER_USERNAME`|
+|`icon_url`|A url that specifies an image to use as the user avatar icon in Slack|`SLACK_NOTIFIER_ICON_URL`|
+|`notify_on`|If set. Possible values `failed`, `passed` or `failed_and_passed_after_failed`. <br />Default is `all`.<br /><ul><li>If set to `failed`, it will only notify on failed builds or deploys.</li><li>If set to `passed`, it will only notify on passed builds or deploys.</li><li>If set to `failed_and_passed_after_failed`, it will only notify on failed builds/deploys or passed after a failed. This requires the `wercker_token` option.</li></ul>|`SLACK_NOTIFIER_NOTIFY_ON`|
+|`branch`|If set, it will only notify on the given branch|`SLACK_NOTIFIER_BRANCH`|
+|`wercker_token`|This is a wercker API token that can access the application.<br/>Required for the`notify_on` value `failed_and_passed_after_failed`.|`SLACK_NOTIFIER_WERCKER_TOKEN`|
+
+## Examples
+
+Some examples
+
+### Simple
+
+```yaml
+build:
+  after-steps:
+    - kingsquare/kelevra-slack-notifier:
+        url: $SLACK_URL
+        channel: notifications
+        username: myamazingbotname
+        branch: master
+```
+
+`SLACK_URL` is set in the pipeline or organisation environment.
+
+### Use organisation defaults for easiest project integration
+
+For easiest integration in to your application's `wercker.yml` add options as environment variables to your organisation. Then you can just:
+
+```yaml
+build:
+  after-steps:
+    - kingsquare/kelevra-slack-notifier
+```
+
+### notify on failed or passed when previous failed
+
+```yaml
+build:
+  after-steps:
+    - kingsquare/kelevra-slack-notifier
+      notify_on: failed_and_passed_after_failed
+```
+
+# Testing
+
+Using the wercker-cli
+
+# License
+
+The MIT License (MIT)
+
+# Changelog
+
+## 0.0.1
+
+- Initial release based on the [official build step](https://github.com/wercker/step-slack)
---
+++ run.sh
@@ -0,0 +1,146 @@
+#!/bin/bash
+# ----------------------------------------------------------------------------------------------------------------------
+#
+# Wercker build step slack notifier
+#
+# ----------------------------------------------------------------------------------------------------------------------
+
+#
+# Fix env expectations
+# This supports setting values through wercker environment and also overriding (or setting) them via the wercker.yml
+#
+# this is also for interchangeability of slack notifiers. i.e. can switch to this one from another based on the
+# official notifier.
+#
+# e.g. Wanting the slack notifier url:
+# 1. WERCKER_KELEVRA_SLACK_NOTIFIER_URL is set via option in wercker.yml as `kelevra-slack-notifier` is the step.
+# 2. if WERCKER_KELEVRA_SLACK_NOTIFIER_URL is not set then SLACK_NOTIFIER_URL is used from ENV (pipeline else organisation)
+# 3. else WERCKER_SLACK_NOTIFIER_URL is empty
+#
+
+WERCKER_SLACK_NOTIFIER_URL=${WERCKER_KELEVRA_SLACK_NOTIFIER_URL:-${SLACK_NOTIFIER_URL}}
+WERCKER_SLACK_NOTIFIER_NOTIFY_ON=${WERCKER_KELEVRA_SLACK_NOTIFIER_NOTIFY_ON:-${SLACK_NOTIFIER_NOTIFY_ON}}
+WERCKER_SLACK_NOTIFIER_CHANNEL=${WERCKER_KELEVRA_SLACK_NOTIFIER_CHANNEL:-${SLACK_NOTIFIER_CHANNEL}}
+WERCKER_SLACK_NOTIFIER_USERNAME=${WERCKER_KELEVRA_SLACK_NOTIFIER_USERNAME:-${SLACK_NOTIFIER_USERNAME}}
+WERCKER_SLACK_NOTIFIER_BRANCH=${WERCKER_KELEVRA_SLACK_NOTIFIER_BRANCH:-${SLACK_NOTIFIER_BRANCH}}
+WERCKER_SLACK_NOTIFIER_WERCKER_TOKEN=${WERCKER_KELEVRA_SLACK_NOTIFIER_WERCKER_TOKEN:-${SLACK_NOTIFIER_WERCKER_TOKEN}}
+
+# check if slack webhook url is present
+if [ -z "$WERCKER_SLACK_NOTIFIER_URL" ]; then
+  fail "Please provide a Slack webhook URL"
+fi
+
+# skip notifications if not interested in passed builds or deploys
+if [ "$WERCKER_SLACK_NOTIFIER_NOTIFY_ON" = "failed" ]; then
+	if [ "$WERCKER_RESULT" = "passed" ]; then
+		return 0
+	fi
+fi
+
+# skip notifications if not on the right branch
+if [ -n "$WERCKER_SLACK_NOTIFIER_BRANCH" ]; then
+    if [ "$WERCKER_SLACK_NOTIFIER_BRANCH" != "$WERCKER_GIT_BRANCH" ]; then
+        return 0
+    fi
+fi
+
+# skip notifications if not interested in passed builds or deploys
+if [[ "$WERCKER_RESULT" = "passed" ] && [ ${WERCKER_SLACK_NOTIFIER_NOTIFY_ON} = "failed_and_passed_after_failed" ]]; then
+    if [ -z ${WERCKER_SLACK_NOTIFIER_WERCKER_TOKEN} ]; then
+        fail "No Wercker API token is specified."
+    fi
+
+    CURL="curl -H "Authorization: Bearer: ${WERCKER_SLACK_NOTIFIER_WERCKER_TOKEN}" https://app.wercker.com/api/v3"
+
+    # get the current pipeline-id (as is not available in the wercker env)
+    WERCKER_PIPELINE_ID=$(${CURL}/runs/${WERCKER_RUN_ID} | ./jq .pipeline.id )
+
+    # get previous run result for the current pipeline & branch
+    WERCKER_PREVIOUS_RESULT=$(${CURL}/runs/?&limit=1&status=finished&branch=${WERCKER_GIT_BRANCH}&pipelineId=${WERCKER_PIPELINE_ID} | ./jq .result)
+
+    if [ ${WERCKER_PREVIOUS_RESULT} != 'failed']; then
+        return 0;
+    fi
+fi
+
+# check if a '#' was supplied in the channel name
+if [ "${WERCKER_SLACK_NOTIFIER_CHANNEL:0:1}" = '#' ]; then
+  export WERCKER_SLACK_NOTIFIER_CHANNEL=${WERCKER_SLACK_NOTIFIER_CHANNEL:1}
+fi
+
+# if no username is provided use the default - werckerbot
+if [ -z "$WERCKER_SLACK_NOTIFIER_USERNAME" ]; then
+  export WERCKER_SLACK_NOTIFIER_USERNAME=werckerbot
+fi
+
+# if no icon-url is provided for the bot use the default wercker icon
+if [ -z "$WERCKER_SLACK_NOTIFIER_ICON_URL" ]; then
+  export WERCKER_SLACK_NOTIFIER_ICON_URL="https://secure.gravatar.com/avatar/a08fc43441db4c2df2cef96e0cc8c045?s=140"
+fi
+
+# check if this event is a build or deploy
+if [ -n "$DEPLOY" ]; then
+  # its a deploy!
+  export ACTION="deploy ($WERCKER_DEPLOYTARGET_NAME)"
+  export ACTION_URL=$WERCKER_DEPLOY_URL
+else
+  # its a build!
+  export ACTION="build"
+  export ACTION_URL=$WERCKER_BUILD_URL
+fi
+
+export MESSAGE="<$ACTION_URL|$ACTION> for $WERCKER_APPLICATION_NAME by $WERCKER_STARTED_BY has $WERCKER_RESULT on branch $WERCKER_GIT_BRANCH"
+export FALLBACK="$ACTION for $WERCKER_APPLICATION_NAME by $WERCKER_STARTED_BY has $WERCKER_RESULT on branch $WERCKER_GIT_BRANCH"
+export COLOR="good"
+
+if [ "$WERCKER_RESULT" = "failed" ]; then
+  export MESSAGE="$MESSAGE at step: $WERCKER_FAILED_STEP_DISPLAY_NAME"
+  export FALLBACK="$FALLBACK at step: $WERCKER_FAILED_STEP_DISPLAY_NAME"
+  export COLOR="danger"
+fi
+
+# construct the json
+json="{"
+
+# channels are optional, dont send one if it wasnt specified
+if [ -n "$WERCKER_SLACK_NOTIFIER_CHANNEL" ]; then
+    json=$json"\"channel\": \"#$WERCKER_SLACK_NOTIFIER_CHANNEL\","
+fi
+
+json=$json"
+    \"username\": \"$WERCKER_SLACK_NOTIFIER_USERNAME\",
+    \"icon_url\":\"$WERCKER_SLACK_NOTIFIER_ICON_URL\",
+    \"attachments\":[
+      {
+        \"fallback\": \"$FALLBACK\",
+        \"text\": \"$MESSAGE\",
+        \"color\": \"$COLOR\"
+      }
+    ]
+}"
+
+# post the result to the slack webhook
+RESULT=$(curl -d "payload=$json" -s "$WERCKER_SLACK_NOTIFIER_URL" --output "$WERCKER_STEP_TEMP"/result.txt -w "%{http_code}")
+cat "$WERCKER_STEP_TEMP/result.txt"
+
+if [ "$RESULT" = "500" ]; then
+  if grep -Fqx "No token" "$WERCKER_STEP_TEMP/result.txt"; then
+    fail "No token is specified."
+  fi
+
+  if grep -Fqx "No hooks" "$WERCKER_STEP_TEMP/result.txt"; then
+    fail "No hook can be found for specified subdomain/token"
+  fi
+
+  if grep -Fqx "Invalid channel specified" "$WERCKER_STEP_TEMP/result.txt"; then
+    fail "Could not find specified channel for subdomain/token."
+  fi
+
+  if grep -Fqx "No text specified" "$WERCKER_STEP_TEMP/result.txt"; then
+    fail "No text specified."
+  fi
+fi
+
+if [ "$RESULT" = "404" ]; then
+  fail "Subdomain or token not found."
+fi
